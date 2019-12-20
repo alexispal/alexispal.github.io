87096f26ce8ea0c53dea7182d2395210c49eb2a1
alexispal <alexispalf@gmail.com>
2019-12-20 15:40:29 -0600

    website completed and validated

--- lesson14/js/scripts.js
+++ lesson14/js/scripts.js
@@ -54,3 +54,36 @@ fetch(requestURL)
       }
     }
   });
+
+  //Images Lazy Loading
+const loadImages = document.querySelectorAll("[data-src]");
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+function firstLoadImg(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+    img.src = src;
+    img.onload = () => {
+        img.removeAttribute("data-src");
+    }
+};
+
+const imgObserve = new IntersectionObserver((entries, imgObserve) => {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            firstLoadImg(entry.target);
+            imgObserve.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+loadImages.forEach(image => {
+    imgObserve.observe(image);
+});
